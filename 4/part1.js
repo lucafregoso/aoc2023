dc0ae2134abcc7c2a613c13f9b5db3612487f9c2
Luca Fregoso <luca.fregoso@gmail.com>
2023-12-04 23:03:52 +0100

    day 4

--- 4/part1.js
+++ 4/part1.js
@@ -1,27 +1,40 @@
 const { importFromTextFile } = require("../lib/index");
 
-const inputValues = importFromTextFile(`${__dirname}/test-input-1.txt`);
+const inputValues = importFromTextFile(`${__dirname}/input-1.txt`);
 
-let winningNumbers = [];
-let ownNumbers = [];
+let score = 0;
+
+const getNumbersFromString = (string) =>
+  string
+    .trim()
+    .split(" ")
+    .map((string) => string.trim())
+    .filter((string) => string != "");
+
+const calculateScore = (times) => {
+  if (times > 0) {
+    return calculateScore(times - 1) * 2;
+  } else if (times == 0) {
+    return 1;
+  }
+  return 0;
+};
+
+let scratchCards = [];
 
 for (lineNumbers of inputValues) {
   const splitNumbers = lineNumbers.split(":")[1].split("|");
-  winningNumbers.push(
-    splitNumbers[0]
-      .trim()
-      .split(" ")
-      .map((string) => string.trim())
-  );
-  ownNumbers.push(
-    splitNumbers[1]
-      .trim()
-      .split(" ")
-      .map((string) => parseInt(string.trim()))
-  );
+  scratchCards.push([
+    getNumbersFromString(splitNumbers[0]),
+    getNumbersFromString(splitNumbers[1]),
+  ]);
 }
 
-console.log({
-  winningNumbers,
-  ownNumbers,
+scratchCards.map((scratchCard) => {
+  const winningNumbers = scratchCard[0].filter((number) =>
+    scratchCard[1].includes(number)
+  );
+  score += calculateScore(winningNumbers.length - 1);
 });
+
+console.log(score);
--- 4/part2.js
+++ 4/part2.js
@@ -1,252 +1,51 @@
 const { importFromTextFile } = require("../lib/index");
 
-// const inputValues = importFromTextFile(`${__dirname}/test-input-1.txt`);
 const inputValues = importFromTextFile(`${__dirname}/input-1.txt`);
+// const inputValues = importFromTextFile(`${__dirname}/test-input-1.txt`);
 
-const getAdiacentItems = (inputValues, lineIndex, rowIndex) => {
-  let adiacentItems = [];
+let score = 0;
 
-  // Check if there is a line above the current line
-  if (lineIndex > 0) {
-    // Check if there is a row to the left of the current row
-    if (rowIndex > 0) {
-      // Add the item to the top-left of the current position
-      adiacentItems.push({
-        line: lineIndex - 1,
-        row: rowIndex - 1,
-        value: inputValues[lineIndex - 1][rowIndex - 1],
-      });
-    }
+const getNumbersFromString = (string) =>
+  string
+    .trim()
+    .split(" ")
+    .map((string) => string.trim())
+    .filter((string) => string != "");
 
-    // Add the item above the current position
-    adiacentItems.push({
-      line: lineIndex - 1,
-      row: rowIndex,
-      value: inputValues[lineIndex - 1][rowIndex],
-    });
-
-    // Check if there is a row to the right of the current row
-    if (rowIndex < inputValues[lineIndex].length - 1) {
-      // Add the item to the top-right of the current position
-      adiacentItems.push({
-        line: lineIndex - 1,
-        row: rowIndex + 1,
-        value: inputValues[lineIndex - 1][rowIndex + 1],
-      });
-    }
+const calculateScore = (times) => {
+  if (times > 0) {
+    return calculateScore(times - 1) * 2;
+  } else if (times == 0) {
+    return 1;
   }
-
-  // Check if there is a row to the left of the current row
-  if (rowIndex > 0) {
-    // Add the item to the left of the current position
-    adiacentItems.push({
-      line: lineIndex,
-      row: rowIndex - 1,
-      value: inputValues[lineIndex][rowIndex - 1],
-    });
-  }
-
-  // Check if there is a row to the right of the current row
-  if (rowIndex < inputValues[lineIndex].length - 1) {
-    // Add the item to the right of the current position
-    adiacentItems.push({
-      line: lineIndex,
-      row: rowIndex + 1,
-      value: inputValues[lineIndex][rowIndex + 1],
-    });
-  }
-
-  // Check if there is a line below the current line
-  if (lineIndex < inputValues.length - 1) {
-    // Check if there is a row to the left of the current row
-    if (rowIndex > 0) {
-      // Add the item to the bottom-left of the current position
-      adiacentItems.push({
-        line: lineIndex + 1,
-        row: rowIndex - 1,
-        value: inputValues[lineIndex + 1][rowIndex - 1],
-      });
-    }
-
-    // Add the item below the current position
-    adiacentItems.push({
-      line: lineIndex + 1,
-      row: rowIndex,
-      value: inputValues[lineIndex + 1][rowIndex],
-    });
-
-    // Check if there is a row to the right of the current row
-    if (rowIndex < inputValues[lineIndex].length - 1) {
-      // Add the item to the bottom-right of the current position
-      adiacentItems.push({
-        line: lineIndex + 1,
-        row: rowIndex + 1,
-        value: inputValues[lineIndex + 1][rowIndex + 1],
-      });
-    }
-  }
-
-  return adiacentItems;
+  return 0;
 };
 
-const isSymbol = (char) => {
-  return char === "*";
-};
+let scratchCards = [];
 
-const calculateSum = (values) => {
-  if (!Array.isArray(values) && typeof values === "object") {
-    values = Object.values(values);
-  }
+for (lineNumbers of inputValues) {
+  const splitNumbers = lineNumbers.split(":")[1].split("|");
+  scratchCards.push([
+    getNumbersFromString(splitNumbers[0]),
+    getNumbersFromString(splitNumbers[1]),
+  ]);
+}
 
-  return values.reduce(
-    (a, b) => Object.values(b).reduce((c, d) => c * d) + a,
-    0
+let cardInstancesQty = Array(scratchCards.length).fill(1);
+
+scratchCards.map((scratchCard, index) => {
+  const winningNumbers = scratchCard[0].filter((number) =>
+    scratchCard[1].includes(number)
   );
-};
+  // console.log(`Card ${index} has ${winningNumbers.length} winning numbers`);
 
-const filterInputValuesLine = (inputValues, lineIndex) => {
-  let filteredInputValuesCoordinates = [];
-  for (const rowIndex in inputValues[lineIndex]) {
-    if (!isFinite(inputValues[lineIndex][rowIndex])) {
-      continue;
-    }
-
-    const adiacentItems = getAdiacentItems(
-      inputValues,
-      lineIndex,
-      parseInt(rowIndex)
-    );
-
-    const adiacentSymbols = adiacentItems.filter((item) =>
-      isSymbol(item.value)
-    );
-
-    if (adiacentSymbols.length) {
-      filteredInputValuesCoordinates.push([
-        lineIndex,
-        parseInt(rowIndex),
-        adiacentSymbols,
-      ]);
-    }
-  }
-  return filteredInputValuesCoordinates;
-};
-const getInputValueAndCoordinates = (inputValues, lineIndex, rowIndex) => {
-  let wholeValue = inputValues[lineIndex][rowIndex];
-  let tmpStartIndex = rowIndex - 1;
-  while (
-    tmpStartIndex >= 0 &&
-    isFinite(inputValues[lineIndex][tmpStartIndex])
+  for (
+    let cardIdIncrement = 0;
+    cardIdIncrement < winningNumbers.length;
+    cardIdIncrement++
   ) {
-    wholeValue = inputValues[lineIndex][tmpStartIndex] + wholeValue;
-    tmpStartIndex--;
+    cardInstancesQty[index + 1 + cardIdIncrement] += cardInstancesQty[index];
   }
-  let tmpEndIndex = rowIndex + 1;
-  while (
-    tmpEndIndex < inputValues[lineIndex].length &&
-    isFinite(inputValues[lineIndex][tmpEndIndex])
-  ) {
-    wholeValue = wholeValue + inputValues[lineIndex][tmpEndIndex];
-    tmpEndIndex++;
-  }
-  return {
-    line: lineIndex,
-    start: tmpStartIndex + 1,
-    end: tmpEndIndex - 1,
-    value: wholeValue,
-  };
-};
-const filterInputValues = (inputValues) => {
-  let filteredInputValuesCoordinates = [];
-  let uniqueFilteredInputValues = {};
-  for (const inputLineIndex in inputValues) {
-    filteredInputValuesCoordinates[inputLineIndex] = filterInputValuesLine(
-      inputValues,
-      parseInt(inputLineIndex)
-    );
-  }
-  filteredInputValuesCoordinates = filteredInputValuesCoordinates.filter(
-    (lineIndex) => lineIndex.length
-  );
-  const doubleFilteredInputValuesCoordinates = {};
-  filteredInputValuesCoordinates = filteredInputValuesCoordinates.forEach(
-    (lineItems) => {
-      return lineItems.map((inputWithAdiacentSymbol) => {
-        // const symbolInfos =
-        inputWithAdiacentSymbol[2].forEach((symbolInfo) => {
-          if (
-            !doubleFilteredInputValuesCoordinates.hasOwnProperty(
-              `${symbolInfo.line}-${symbolInfo.row}-${symbolInfo.value}`
-            )
-          ) {
-            doubleFilteredInputValuesCoordinates[
-              `${symbolInfo.line}-${symbolInfo.row}-${symbolInfo.value}`
-            ] = [];
-          }
-          if (
-            !doubleFilteredInputValuesCoordinates[
-              `${symbolInfo.line}-${symbolInfo.row}-${symbolInfo.value}`
-            ].includes(
-              `${inputWithAdiacentSymbol[0]}-${inputWithAdiacentSymbol[1]}`
-            )
-          ) {
-            doubleFilteredInputValuesCoordinates[
-              `${symbolInfo.line}-${symbolInfo.row}-${symbolInfo.value}`
-            ].push(
-              `${inputWithAdiacentSymbol[0]}-${inputWithAdiacentSymbol[1]}`
-            );
-          }
-          // return [
-          //   `${symbolInfo.line}-${symbolInfo.row}-${symbolInfo.value}`,
-          //   `${inputWithAdiacentSymbol[0]}-${inputWithAdiacentSymbol[1]}`,
-          // ];
-        });
-        // return symbolInfos;
-      });
-    }
-  );
-  for (const [symbolInfos, itemsInfos] of Object.entries(
-    doubleFilteredInputValuesCoordinates
-  )) {
-    let parsedLinesIndexes = [];
-    let tmpUniqueFilteredInputValues = {};
-    itemsInfos.forEach((itemInfo) => {
-      const splitItemInfo = itemInfo.split("-");
+});
 
-      // if (!parsedLinesIndexes.includes(splitItemInfo[0])) {
-      // parsedLinesIndexes.push(splitItemInfo[0]);
-      let wholeInputValueAndCoordinates = getInputValueAndCoordinates(
-        inputValues,
-        parseInt(splitItemInfo[0]),
-        parseInt(splitItemInfo[1])
-      );
-      if (
-        wholeInputValueAndCoordinates &&
-        !tmpUniqueFilteredInputValues.hasOwnProperty(
-          `${wholeInputValueAndCoordinates.line}-${wholeInputValueAndCoordinates.start}`
-        )
-      ) {
-        tmpUniqueFilteredInputValues[
-          `${wholeInputValueAndCoordinates.line}-${wholeInputValueAndCoordinates.start}`
-          // splitItemInfo[0] + "--" + splitItemInfo[1]
-        ] = parseInt(wholeInputValueAndCoordinates.value);
-      }
-      // }
-    });
-    console.log({ tmpUniqueFilteredInputValues });
-
-    if (Object.keys(tmpUniqueFilteredInputValues).length >= 2) {
-      uniqueFilteredInputValues = {
-        ...uniqueFilteredInputValues,
-        [`${symbolInfos}`]: { ...tmpUniqueFilteredInputValues },
-      };
-    }
-  }
-
-  return uniqueFilteredInputValues;
-};
-
-let filteredInputValues = filterInputValues(inputValues);
-console.log(filteredInputValues);
-
-console.log(calculateSum(filteredInputValues));
+console.log(cardInstancesQty.reduce((a, b) => a + b, 0));
